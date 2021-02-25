e3a0d834cfe4c8c1f59d1593c5424d3ccc9bffcd
adivigo <khalishtama25@gmail.com>
2021-02-25 20:51:42 +0700

    adivigo

--- models/coursecontent.js
+++ models/coursecontent.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       CourseContent.belongsTo(models.Course, {foreignKey: "CourseId"})
+      CourseContent.hasMany(models.UserCourse, {foreignKey: "courseContentId"})
     }
   };
   CourseContent.init({
