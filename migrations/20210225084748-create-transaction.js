d5e46915788105e2b4b17a8ca8631c4edf5ecc23
adivigo <khalishtama25@gmail.com>
2021-02-25 16:13:21 +0700

    adivigo

---
+++ migrations/20210225084748-create-transaction.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      studentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Students',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Transactions');
+  }
+};
---
+++ models/coursecontent.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CourseContent extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  CourseContent.init({
+    course_name: DataTypes.STRING,
+    release_year: DataTypes.STRING,
+    duration: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    discount: DataTypes.INTEGER,
+    CourseId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'CourseContent',
+  });
+  return CourseContent;
+};
